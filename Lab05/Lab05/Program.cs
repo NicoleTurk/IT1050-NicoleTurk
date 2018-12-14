6693e9e7f23a67dcc776ee08efe3981bcf7f5573
Nicole Turk <S00952239@acad.tri-c.edu>
2018-12-13 21:10:25 -0500

    I like to change things.

---
+++ Lab05/Lab05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //2. A. Scope: double height. double width.() B. Static vs. Non-Static: Static is not used in this example, as it would be located after Public. Static determines if we can call it without creating an object. C. Return Type: Determines the type of value returned, if any. Double. D. Method Name: getArea. Where you get the information. E. Parameters: Public. Determines restrictions on the code, none in public. F. Method Body: return height * width. Code that gets executed  3. The difference between a user-defined method and a method provided by a framework is that a user-defined method is one that is custom made. A method provided by the framework is essentially stock. When creating a user-defined method you should determine whether or not you even need one, if a stock method might be of better use. 4. The difference between a static and non-static method is that non-static requires an object to call them. Static cannot be instantiated. 
+        }
+    }
+}            
+namespace Dog       
+{
+    class Program
+    {
+        public string bark;
+        public static void bark();
+            {
+                Dog myDog=new Dog(bark);
+                myDog.Name="Fido";
+
+                // To do: uncomment the following lines of code to call the methods
+                myDog.bark()
+                myDog.doTrick("Fetch")
+            }
+    }
+}
+    
+
---
+++ Midterm/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = 2;
+            while (q < 128) //If you write while (q <= 128) it'll write the full thing, but if you tweak it then it won't go past 64.
+            {
+                Console.WriteLine("[{0}]", q);
+                q *= 2;
+            }
+
+            for (int x = 49; x >= 1;)
+            {
+                Console.Write("{0}", x);
+                x--;
+                if (x >= 1)
+                    Console.Write(",");
+            }
+            Console.WriteLine(""); // I need space for activities.
+
+            int i = 1;
+            while (i <= 21)
+            {
+                Console.Write("{0} ", i);
+                i = i + 2;
+            }
+
+            // 7. It posts the * once, because it runs sequentially. If you write it as a while statement, it would function as intended and never print a *.
+
+            bool icyRain = false;
+            bool tornadoWarning = false;
+            string name = "Let's go outside";
+
+            if ((icyRain) & (tornadoWarning));
+            else
+                Console.WriteLine("{0}", name);
+
+                //int f = 6;
+                //Boolean keepLooping = true;
+                // while (keepLooping)
+                //{
+                //    if (f <= 5)
+                //  keepLooping = false;
+                //   f++;
+                //  Console.WriteLine("I can't stop running.");
+                //  }
+
+
+
+            }
+        }
+    }
